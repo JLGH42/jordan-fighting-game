f550112529f9e863c652b8d7e53773839679403f
Jordan <jlprogrammer_98@hotmail.com>
2023-06-24 14:46:53 +0100

    added w&h properties | object detection on canvas

--- script.js
+++ script.js
@@ -7,51 +7,69 @@ canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.4;
 class Sprite {
-  constructor({ position, velocity, colour }) {
+  constructor({ position, velocity, colour, height, width }) {
     this.colour = colour;
     this.position = position;
     this.velocity = velocity;
+    this.height = height;
+    this.width = width;
   }
 
   draw(colour) {
     ctx.fillStyle = colour;
-    ctx.fillRect(this.position.x, this.position.y, 50, 150);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
     this.draw(this.colour);
-    this.position.y = this.position.y + this.velocity.y;
+    //velocity
+    this.position.y += this.velocity.y;
+
+    //stopping object when it reaches the ground
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    }
+
+    //gravity
+    else {
+      this.velocity.y += gravity;
+    }
   }
 }
 
 const player = new Sprite({
   colour: "red",
+  height: 150,
+  width: 50,
   position: {
     x: 0,
     y: 0,
   },
   velocity: {
     x: 0,
-    y: 1.4,
+    y: 0,
   },
 });
 
 const enemy = new Sprite({
   colour: "blue",
+  height: 150,
+  width: 50,
   position: {
     x: 975,
     y: 0,
   },
   velocity: {
     x: 0,
-    y: 1.4,
+    y: 0,
   },
 });
 
 function animate() {
   window.requestAnimationFrame(animate);
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
