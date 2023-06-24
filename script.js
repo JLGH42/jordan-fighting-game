fba8f99104927f3369a7654952a1030ea10670fb
Jordan <jlprogrammer_98@hotmail.com>
2023-06-24 10:58:04 +0100

    created a new Sprite class and instance for player

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ canvas.width = 1024;
 canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor( position ) {
+    this.position = position;
+  }
+
+  draw() {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
+
+player.draw();
+
+console.log(player)
