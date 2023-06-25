ca5df3356e621f9d0509e363e45bf7143ccf5ba9
Jordan <jlprogrammer_98@hotmail.com>
2023-06-25 07:46:18 +0100

    refactored file & added speed and jump height

--- script.js
+++ script.js
@@ -88,8 +88,6 @@ const keys = {
   },
 };
 
-let lastKey;
-
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = "black";
@@ -99,17 +97,17 @@ function animate() {
   enemy.velocity.x = 0;
 
   //player movement
-  if (keys.a.pressed && lastKey === "a") {
-    player.velocity.x = -1;
-  } else if (keys.d.pressed && lastKey === "d") {
-    player.velocity.x = 1;
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
   }
 
   //enemy movement
   if (keys.arrowR.pressed && enemy.lastKey === "ArrowRight") {
-    enemy.velocity.x = 1;
+    enemy.velocity.x = 5;
   } else if (keys.arrowL.pressed && enemy.lastKey === "ArrowLeft") {
-    enemy.velocity.x = -1;
+    enemy.velocity.x = -5;
   }
 
   player.update();
@@ -124,14 +122,14 @@ window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "d":
       keys.d.pressed = true;
-      lastKey = "d";
+      player.lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
-      lastKey = "a";
+      player.lastKey = "a";
       break;
     case "w":
-      player.velocity.y = -10;
+      player.velocity.y = -15;
       break;
     case "ArrowRight":
       keys.arrowR.pressed = true;
@@ -142,7 +140,7 @@ window.addEventListener("keydown", (e) => {
       enemy.lastKey = "ArrowLeft";
       break;
     case "ArrowUp":
-      enemy.velocity.y = -10;
+      enemy.velocity.y = -15;
       break;
   }
 });
